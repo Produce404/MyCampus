e007d1d80d25074a0f924679440a1c4c668f81de
闫开宇 <774353858@qq.com>
2018-07-08 14:35:57 +0800

    主界面
    1

--- app/src/main/java/com/example/hp/mycampus/activity/MainActivity.java
+++ app/src/main/java/com/example/hp/mycampus/activity/MainActivity.java
@@ -1,16 +1,51 @@
 package com.example.hp.mycampus.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.hp.mycampus.R;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton select_course;
+    private ImageButton select_score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //设置课表查询按钮的监听
+        select_course=(ImageButton) findViewById(R.id.imageButton21);
+        select_course.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //跳转
+                Intent intent = new Intent(MainActivity.this, ScoreActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
+        //设置成绩查询按钮的监听
+        select_score=(ImageButton) findViewById(R.id.imageButton20);
+        select_score.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //跳转
+                Intent intent = new Intent(MainActivity.this, CourseActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
+
